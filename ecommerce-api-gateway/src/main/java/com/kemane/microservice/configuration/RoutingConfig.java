43b31308db942609193d3eb6f3fa9ee379205679
Kemane AMH <kdonfack@amhconsulting-group.com>
2023-04-01 21:23:14 +0100

    update

--- ecommerce-api-gateway/src/main/java/com/kemane/microservice/configuration/RoutingConfig.java
+++ ecommerce-api-gateway/src/main/java/com/kemane/microservice/configuration/RoutingConfig.java
@@ -9,8 +9,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RoutingConfig {
 
-    @Value("${host}")
-    private String host;
+    @Value("hostproduct")
+    private String hostproduct;
+    @Value("${hostuser}")
+    private String hostuser;
+    @Value("${hostsale}")
+    private String hostsale;
+    @Value("${hostorder}")
+    private String hostorder;
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
@@ -18,26 +24,26 @@ public class RoutingConfig {
                 //swagger documentation routes
                 .route("ecommerce-product-service", r ->r.path("/api/v1/docs/products/**")
                         .filters(f -> f.rewritePath("^/api/v1/docs/products", ""))
-                        .uri(host+":8083/"))
+                        .uri(hostproduct))
                 .route("ecommerce-user-service", r ->r.path("/api/v1/docs/users/**")
                         .filters(f -> f.rewritePath("^/api/v1/docs/users", ""))
-                        .uri(host+":8084/"))
+                        .uri(hostuser))
                 .route("ecommerce-sale-service", r ->r.path("/api/v1/docs/sales/**")
                         .filters(f -> f.rewritePath("^/api/v1/docs/sales", ""))
-                        .uri(host+":8085/"))
+                        .uri(hostsale))
                 .route("ecommerce-order-service", r ->r.path("/api/v1/docs/orders/**")
                         .filters(f -> f.rewritePath("^/api/v1/docs/orders", ""))
-                        .uri(host+":8086/"))
+                        .uri(hostorder))
 
                 // routes
                 .route("ecommerce-product-service", r ->r.path("/api/v1/products/**")
-                        .uri(host+":8083/"))
+                        .uri(hostproduct))
                 .route("ecommerce-user-service", r ->r.path("/api/v1/users/**")
-                        .uri(host+":8084/"))
+                        .uri(hostuser))
                 .route("ecommerce-sale-service", r ->r.path("/api/v1/sales/**")
-                        .uri(host+":8085/"))
+                        .uri(hostsale))
                 .route("ecommerce-order-service", r ->r.path("/api/v1/orders/**")
-                        .uri(host+":8086/"))
+                        .uri(hostorder))
                 .build();
     }
 }
