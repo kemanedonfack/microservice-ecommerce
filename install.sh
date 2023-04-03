3579e529b8c5a1f34f98d93cd8cac885a8553e9e
Kemane AMH <kdonfack@amhconsulting-group.com>
2023-04-03 18:35:39 +0100

    installation script

---
+++ install.sh
@@ -0,0 +1,67 @@
+
+apt-get -y update && apt-get -y upgrade
+
+curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+sudo sysctl --system
+
+lsmod | grep br_netfilter
+lsmod | grep overlay
+
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+sudo vim /etc/containerd/config.toml
+systemctl restart containerd.service
+systemctl stop firewalld
+
+kubeadm config images pull
+
+kubeadm init --apiserver-advertise-address=<PRIVATE_IP> --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl get pods --all-namespaces
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+helm install mysqldb mysql
+helm install config config-server
+helm install registry registry-service
+helm install gateway api-gateway
+helm install order order-service
+helm install product product-service
+helm install sale sale-service
+helm install user user-service
+
+
+
+sudo -i
+swapoff -a
+exit
+strace -eopenat kubectl version
